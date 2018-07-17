074c4ff9258947c4fc133e89015789310f79d1d9
Nicholas Munson <nicholas.t.munson@gmail.com>
2018-07-17 11:21:03 -0600

    add react-native-flux

--- doggy_business/App.js
+++ doggy_business/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Dashboard from './app/Dashboard.js'
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello world</Text>
+        <Text style={styles.welcome}>Hello world suck my taint</Text>
       </View>
     );
   }
@@ -13,9 +14,16 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    top:"3%",
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a5d9f6',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  welcome:{
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    bottom:"3%",
+  }
 });
---
+++ doggy_business/app/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Actions } from 'react-native-router-flux'
+
+const Dashboard = () =>{
+    return (
+        <View style={styles.container}>
+            <Text style={styles.welcome}>Hello world suck my taint again</Text>
+        </View>
+    )
+
+}
+
+export default Dashboard

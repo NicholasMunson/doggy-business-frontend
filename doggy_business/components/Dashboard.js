73c4dcfe851c8e06a43e0b0605dfb616bcc4079d
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-07-23 14:04:25 -0600

    data rendering in the middle of dash

--- doggy_business/components/Dashboard.js
+++ doggy_business/components/Dashboard.js
@@ -1,21 +1,27 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View, ScrollView} from 'react-native'
 import { Actions } from 'react-native-router-flux'
 import DogCard from './DogCard.js'
+import { Container, Content, Card, CardItem, Text, Icon, Right } from 'native-base';
 // console.log(dogProfile);
 
 
 const Dashboard = (props) =>{
-    const dogData = !props.loadData === true ? console.log("loading...") :  props.dogDataInfo.profile.map(dog => <DogCard key={dog.id} />)
+    const dogData = !props.loadData === true ? console.log("loading...") :  props.dogDataInfo.profile.map(dog => <DogCard key={dog.id} style={styles.dogCard} dogProfile={dog}/>)
     console.log(dogData)
 
     
     return (
-        <View style={styles.container}>
-            {dogData}
-        </View>
+        <ScrollView>
+            <Container>
+                <Content>
+                    <Card>
+                    {dogData}
+                    </Card>
+                </Content>
+            </Container>
+        </ScrollView>
     )
-
 }
 const styles = StyleSheet.create({
     container: { 
@@ -24,9 +30,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    welcome: {
+    dogCard: {
+        width:100,
         fontSize: 20,
         textAlign: 'center',
+    
     }
 });
 export default Dashboard
--- doggy_business/components/DogCard.js
+++ doggy_business/components/DogCard.js
@@ -1,17 +1,54 @@
 import React from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { Container, Content, Card, CardItem, Text, Icon, Right } from 'native-base';
 
 const DogCard = (props) => {
-    console.log(props);
-    
-    
-    
+    let dog = props.dogProfile
     return (
-        <View>
-            <Text>Dog Profile Card</Text>
-        </View>
-    )
-
+        <CardItem style={styles.card}>
+            <Icon />
+            <Text>{dog.name}</Text>
+            <Text>Favorite Toy: {dog.toy}</Text>
+            <Text>Nickname: {dog.nickname}</Text>
+        </CardItem>    
+    ) 
 } 
+const styles = StyleSheet.create({
+    container: { 
+        flex: 1,
+        width: 100,
+        flexDirection: "row",
+        
+        
+        
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+    },
+    card:{
+        flex: 1,
+        flexDirection:"column",
+        justifyContent: "space-between",
+        textAlign: "center"
+    }
+});
 export default DogCard
 {/* <Text>{dogProfile.name}</Text> */}
+// <Container>
+//     <Content>
+//         <Card>
+//             <CardItem>
+//                 <Icon />
+//                 <Text>{dog.name}</Text>
+//                 <Right>
+//                 <Text>Favorite Toy: {dog.toy}</Text>
+//                 </Right>
+//                 <Text>Nickname: {dog.nickname}</Text>
+//             </CardItem>
+//         </Card>
+//     </Content>
+// </Container>
+// <View style={styles.container}>
+//     <Text style={styles.card}>{dog.name}</Text>
+// </View>

a5a4eae76c0b653942e7b749e3e93cfab319da14
Nicholas Munson <nicholasmunson@Nicholass-MacBook-Pro.local>
2018-07-24 10:15:18 -0600

    add style to Auth

--- doggy_business/components/Auth.js
+++ doggy_business/components/Auth.js
@@ -6,44 +6,62 @@ const Auth = (props) => {
     console.log(props.handleAuth);
     
     return(
-        <ImageBackground source={require('./images/dog.png')} style={{width:"100%", height:"100%"}}>
-            <Content style={styles.container}>
-                <Text style={styles.spacing} />  
-                <Form style={styles.form}>
-                    <Item>
-                        <Input placeholder="Username" />
-                    </Item>
-                    <Item last>
-                        <Input placeholder="Password" />
-                    </Item>
-                </Form>
-                <Button block primary onPress={() => props.handleAuth(true)} >
-                    <Text>Login</Text>
-                </Button> 
-                <Button block primary >
-                    <Text>Sign up</Text>
-                </Button>     
-                <Text style={styles.spacing} />    
-            </Content>
-        </ImageBackground>
+        <Container>
+            <ImageBackground source={require('./images/dog.png')} style={styles.background}>
+                <Content style={styles.container}>
+                    <Text style={styles.spacing}>Welcome to Doggy Business</Text>  
+                    <Form style={styles.form}>
+                        <Item>
+                            <Input placeholder="Username" style={styles.input}  required/>
+                        </Item>
+                        <Item last>
+                            <Input placeholder="Password"  style={styles.input}   required/>
+                        </Item>
+                    </Form>
+                    <Button block primary style={styles.btn}  onPress={() => props.handleAuth(true)} >
+                        <Text>Login</Text>
+                    </Button> 
+                    <Button block primary style={styles.btn} >
+                        <Text>Sign up</Text>
+                    </Button>     
+                    <Text style={styles.spacing} />    
+                </Content>
+            </ImageBackground>
+        </Container>
     )
     
 }
 const styles= StyleSheet.create({
     container:{
         flex: 1,
-        paddingTop:"50%",
-
-
+        backgroundColor:"rgba(0, 0, 0, 0.5)",
+    },
+    background:{
+        width:"100%", 
+        height:"100%",
     },
     spacing:{
-        flex: 2,
+        justifyContent: "center",
+        textAlign: "center",
+        color:"#372772",
+        fontSize: 50,
+        margin: 20,
+        shadowColor: "white",
+        shadowOpacity: 10,
+        shadowRadius: 4,
     },
     form:{
-        paddingTop:"50%",
         flex: .5,
         height:"20%",
-        backgroundColor:"rgba(0,0,0,0.5)",
+        backgroundColor:"#CDD5D1",
+        margin:10,
+    },
+    btn:{
+        margin:10,
+    },
+    input:{
+        backgroundColor:"#CDD5D1",
+        marginRight:10,
     }
 
 })  
